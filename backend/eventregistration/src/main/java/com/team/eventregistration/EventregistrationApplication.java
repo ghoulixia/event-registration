a2ecf4e5db749d13fad2dbc2f76e65f495a2a003
ghoulixia <huyminh06hn@gmail.com>
2026-09-25 16:06:25 +0700

    initial commit

---
+++ backend/eventregistration/src/main/java/com/team/eventregistration/EventregistrationApplication.java
@@ -0,0 +1,13 @@
+package com.team.campusevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampuseventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampuseventApplication.class, args);
+	}
+
+}
---
+++ backend/eventregistration/src/test/java/com/team/eventregistration/EventregistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.campusevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampuseventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
